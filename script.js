7215b243321f25000cfd8af4d9ec9222172caffb
cyprianmbau <mbaucyprian@gmail.com>
2026-05-03 10:06:52 +0300

    setup javascript file though not complete.

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-function findAkanName() {
-    const dateInput = document.findElementById("birthdate").value;
-    const gender =
-        document.querySelector("input[name="gender"]:checked");
-    If(!dateInput//!gender){
+function getAkanName() {
+    const dateOfBirth = document.getElementById("dateOfBirth").value;
+    const gender = document.querySelector("input[name='gender']:checked");
+    If(!dateOfBirth || !gender){
         alert("give birthdate and select your gender.");
-    return;
-}
-const date = newDate(dateInput);
-const day = date.getDay();
-const maleNames = [
-    "kwasi",//sunday
-    "Kwadwo",//monday
-    "kwabena",//tuesday
-]
+        return;
+    }
+    const date = new Date(dateOfBirth);
+    const day = date.getDay();
+    const maleNames = [
+        "kwasi", "Kwadwo", "kwabena", "kwaku", "Yaw", "kofi", "Kwame"
+    ];
+    const femaleNames = [
+        "Akoswa", "Adwoa", "Abenoa", "Akua", "Yaa", "Afua", "Ama"
+    ];
+    let akanName;
+    const dayNames = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    if (gender.value === "male") {
+        akanName = maleNames[day];
+    } else {
+        akanName = femaleNames[day];
+    }
+    document.getElementById("result").innerHTML =
+        `your akan name is <b>${akanName}</b>.<br>
+        you were born on <b>${dayNames[day]}</b>.`;
 }
